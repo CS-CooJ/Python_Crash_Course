2eae34f5e5a2d6c6c93cd4d74d01e0b56cb3503e
CS-CooJ <64668684+CS-CooJ@users.noreply.github.com>
2020-05-14 16:22:34 -0500

    Initial Commit. Chapter 1

---
+++ ch_1/hello_world.py
@@ -0,0 +1,11 @@
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+simple_message = "This will be your simple message now."
+print(simple_message)
+
+simple_message = "This simple message has now changed."
+print(simple_message)
